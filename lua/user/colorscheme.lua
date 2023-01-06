b49450bcde8e0529c7bd3a1dc39a7da0fa9960d8
lvanderlaat <lvmzxc@gmail.com>
2023-01-06 18:16:00 -0500

    changes from earth-c07c30mljyw0.iss.lsa.umich.edu on Fri Jan 6 18:16:00 EST 2023

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -16,49 +16,47 @@ augroup END
 autocmd! bufwritepost colorscheme.lua source %
 ]]
 
+
+
+-- Default options
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = true,    -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = true,   -- Non focused panes set to alternative background
+    module_default = true,  -- Default enable value for modules
+    styles = {              -- Style to be applied to different syntax groups
+      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
     return
 end
-
-
--- -- Default options
--- require('nightfox').setup({
---   options = {
---     -- Compiled file's destination location
---     compile_path = vim.fn.stdpath("cache") .. "/nightfox",
---     compile_file_suffix = "_compiled", -- Compiled file suffix
---     transparent = true,    -- Disable setting background
---     terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
---     dim_inactive = true,   -- Non focused panes set to alternative background
---     module_default = true,  -- Default enable value for modules
---     styles = {              -- Style to be applied to different syntax groups
---       comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
---       conditionals = "NONE",
---       constants = "NONE",
---       functions = "NONE",
---       keywords = "NONE",
---       numbers = "NONE",
---       operators = "NONE",
---       strings = "NONE",
---       types = "NONE",
---       variables = "NONE",
---     },
---     inverse = {             -- Inverse highlight for different types
---       match_paren = false,
---       visual = false,
---       search = false,
---     },
---     modules = {             -- List of various plugins and additional options
---       -- ...
---     },
---   },
---   palettes = {},
---   specs = {},
---   groups = {},
--- })
-
--- setup must be called before loading
--- vim.cmd("colorscheme " .. colorscheme)
