2911630e257fac63f2826c6ae1d239f8f037ed3d
Leonardo van der Laat <laat@MBP14.local>
2023-01-07 10:59:29 -0500

    changes from MBP14.local on Sat Jan 7 10:59:29 EST 2023

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -74,7 +74,6 @@ return packer.startup(
         use 'hrsh7th/cmp-vsnip'
         use 'hrsh7th/vim-vsnip'
 
-
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
         if PACKER_BOOTSTRAP then
