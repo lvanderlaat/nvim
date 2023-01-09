fb0e7c229d9820b1b0237d72f9dd505b20039092
Leonardo van der Laat <laat@MBP14.JOOWIN>
2023-01-09 09:21:38 -0500

    changes from MBP14.JOOWIN on Mon Jan 9 09:21:38 EST 2023

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -14,7 +14,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
-    local bufopts = {noremap=true, silent=true, buffer=bufnr}
+   local bufopts = {noremap=true, silent=true, buffer=bufnr}
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -23,22 +23,15 @@ local on_attach = function(client, bufnr)
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
+    vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format {async=true} end, bufopts)
 end
 
-local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
-}
 require('lspconfig')['pyright'].setup{
     on_attach=on_attach,
-    flags=lsp_flags,
     autostart=true
 }
