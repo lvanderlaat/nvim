4f244429ed3716999622a7dbd872ab201a6b80fe
Leonardo van der Laat <lvmzxc@gmail.com>
2023-01-09 11:32:11 -0500

    yaml ftplugin

---
+++ ftplugin/yaml.lua
@@ -0,0 +1,8 @@
+vim.opt.filetype = 'on'
+vim.opt.colorcolumn = '80'
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.textwidth = 79
+vim.opt.expandtab = true
+vim.opt.autoindent = true
