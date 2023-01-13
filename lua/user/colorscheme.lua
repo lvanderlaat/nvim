4f94edc6039fc275ff25af5b4320841c08752d9c
Leonardo van der Laat <lvmzxc@gmail.com>
2023-01-13 07:23:54 -0500

    changes from gl-login3.arc-ts.umich.edu on Fri Jan 13 07:23:54 EST 2023

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,15 +6,15 @@ require("tokyonight").setup({
     transparent=false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments={italic=true},
-    keywords={italic=true},
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments={italic=true},
+        keywords={italic=true},
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
     },
     sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
